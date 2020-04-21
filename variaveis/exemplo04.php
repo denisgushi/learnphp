6e7f38a5cccc9001f3ee3b885160cdc07c61033c
Denis Gushi <denis.gushi@gmail.com>
2020-04-21 16:24:36 -0300

    comentários no exemplo04.php

--- variaveis/exemplo04.php
+++ variaveis/exemplo04.php
@@ -1,10 +1,12 @@
 <?php
 // colocar na url a uri ?a=123 após o endereço
-$nome = (int)$_GET["a"];
-//var_dump($nome);
+$nome = (int)$_GET["a"];  //O (int) força o tipo de variável se tornar do tipo integer
+// As variáveis pré definidas $_GET e $_POST sempre retornan strings
+
+var_dump($nome);
 
 $ip = $_SERVER["REMOTE_ADDR"];
 
-echo $ip;
+//echo $ip;
 
 ?>
