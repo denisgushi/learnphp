96ac85a772eb3de9de2fa2bbf55e051b864f310c
Denis Gushi <dgushi@wishin.com.br>
2020-04-21 15:41:02 -0300

    Incluido exemplo de variaveis

---
+++ variaveis/exemplo03.php
@@ -0,0 +1,27 @@
+<?php
+////////////////////////////////////////////
+// Tipo Simples
+$nome = "Denis";
+$site = 'www.hcode.com.br';
+
+$ano = 2020;
+$salario = 1000.00;
+$bloqueado = false;
+
+////////////////////////////////////////////
+// Tipo Composto
+
+$frutas = array("abacaxi", "laranja", "manga");
+//echo $frutas[2];
+
+$nascimento = new DateTime();
+//var_dump($nascimento);
+
+////////////////////////////////////////////
+// Tipo Especial
+
+$arquivo = fopen("exemplo03.php");
+var_dump($arquivo);
+
+
+?>
