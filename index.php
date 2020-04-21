68466d0ebbff0ce536450cbe51d3a157f414c35c
Denis Gushi <denis.gushi@gmail.com>
2020-04-21 13:30:32 -0300

    Alterado index.php com comentários e concatenador

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-	echo "Hello world!";
+// Comentário em PHP
+/*
+ * Outra forma de fazer comentários
+ */
+
+$nome = "Denis";
+$sobrenome = "Gushi";
+echo "Hello world!";
+echo "<br>";
+echo $nome . " " . $sobrenome; // o ponto é o simbolo para concatenar strings
 
 ?>
